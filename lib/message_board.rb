5b3622592d7be5ce53a3fedaebedb05214d6ff9d
Daniel Merys <dkmerys@gmail.com>
2020-05-14 10:08:05 -0700

    add equality override test/method and clear test/method and update test

--- lib/message_board.rb
+++ lib/message_board.rb
@@ -1,11 +1,30 @@
 class Board
-  attr_reader :topic
+  attr_reader :topic, :id
+  attr_accessor :topic, :id
   @@boards = {}
   @@total_rows = 0
 
   def initialize(attributes)
     @topic = attributes.fetch(:topic)
-    @id = attributes.fetch(:id || @@total_rows += 1)
+    @id = attributes.fetch(:id) || @@total_rows += 1
+  end
+  
+  def self.all
+    @@boards.values()
+  end
+
+  def self.clear
+    @@boards = {}
+    @@total_rows = 0
+  end
+  
+  def ==(board_to_compare)
+    self.topic() == board_to_compare.topic()
+  end
+
+
+  def save
+    @@boards[self.id] = Board.new({:topic => self.topic, :id => self.id})
   end
 
 end
--- spec/message_board_spec.rb
+++ spec/message_board_spec.rb
@@ -3,15 +3,53 @@ require 'pry'
 require 'message_board'
 
 describe '#Board' do
+
+  before (:each) do
+    Board.clear()
+  end
   
- describe('#save') do
-   it("save a message board") do
-     board = Board.new({:topic => "music", :id => nil})
-     board.save()
-     board2 = Board.new({:topic => "politics", :id => nil})
-     board.save()
-     expect(Board.all).to(eq([board, board2]))
-   end
- end
+  describe('#save') do
+    it("save a message board") do
+      board = Board.new({:topic => "music", :id => nil})
+      board.save()
+      board2 = Board.new({:topic => "politics", :id => nil})
+      board2.save()
+      expect(Board.all).to(eq([board, board2]))
+    end
+  end
+
+  describe('.all') do
+    it("returns an empty array when there are no boards") do
+      expect(Board.all).to(eq([]))
+    end
+  end
+
+  describe('.clear') do
+    it("clears all albums") do
+      board = Board.new({:topic => "music", :id => nil})
+      board.save()
+      board2 = Board.new({:topic => "politics", :id => nil})
+      board2.save()
+      Board.clear()
+      expect(Board.all).to(eq([]))
+    end
+  end
+
+  describe('#==') do
+    it("is the same board if it has the same attributes as another board") do
+      board = Board.new({:topic => "music", :id => nil})
+      board2 = Board.new({:topic => "music", :id => nil})
+      expect(board).to(eq(board2))
+    end
+  end
+
+  describe('#update') do
+    it("updates an board with new topic") do
+      board = Board.new({:topic => "music", :id => nil})   
+      board.save
+      board.update("politics")   
+      expect(board.topic).to(eq("politics"))
+    end
+  end
 
 end
