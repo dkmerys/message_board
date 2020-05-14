d47dc54aab90a6ea2ab3c67247a6b38422440a0d
Daniel Merys <dkmerys@gmail.com>
2020-05-14 09:13:47 -0700

    successfully fail save method test

--- lib/message_board.rb
+++ lib/message_board.rb
@@ -1,1 +1,11 @@
-*
+class Board
+  attr_reader :topic
+  @@boards = {}
+  @@total_rows = 0
+
+  def initialize(attributes)
+    @topic = attributes.fetch(:topic)
+    @id = attributes.fetch(:id || @@total_rows += 1)
+  end
+
+end
--- spec/message_board_spec.rb
+++ spec/message_board_spec.rb
@@ -1,1 +1,17 @@
-*
+require 'rspec'
+require 'pry'
+require 'message_board'
+
+describe '#Board' do
+  
+ describe('#save') do
+   it("save a message board") do
+     board = Board.new({:topic => "music", :id => nil})
+     board.save()
+     board2 = Board.new({:topic => "politics", :id => nil})
+     board.save()
+     expect(Board.all).to(eq([board, board2]))
+   end
+ end
+
+end
